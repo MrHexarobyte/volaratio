ef5d48cc62a26eb9159c557dadc1b658ed1626cf
mrHexarobyte <120273837+MrHexarobyte@users.noreply.github.com>
2023-06-26 17:00:57 +0300

    ver1.0

--- examplescript.py
+++ examplescript.py
@@ -1,14 +1,6 @@
-from main import *
-app = SoftwareRender()
-app.objects.append(Cube3D(app,pg.Color('red'),[1.0,0,0]))
+from volaratio import *
+app = Renderer()
+app.objects.append(Cube3D(app,pg.Color('red'),[1,0,0]))
+app.objects.append(Cube3D(app,pg.Color('red'),[2,0,0]))
 app.objects.append(Cube3D(app,pg.Color('red'),[3,0,0]))
-app.objects.append(Cube3D(app,pg.Color('red'),[4,0,0]))
-app.objects.append(Cube3D(app,pg.Color('red'),[5,0,0]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,0]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,1]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,2]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,3]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,4]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,5]))
-app.objects.append(Cube3D(app,pg.Color('red'),[6,0,6]))
 app.run()
